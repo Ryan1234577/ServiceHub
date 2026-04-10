0e7e3b2788c71cc3d3f7e9c38fb1646f9851a7fa
Ryan Marcos da Silva Costa <ryanmarcosdasilvacosta@gmail.com>
2026-04-10 14:45:58 -0300

    Includes linkaveis

--- aula-pdo.php
+++ aula-pdo.php
@@ -1,58 +1,73 @@
 <?php
+//include_onde: conexão com o script
 include_once "config/conexao.php";
  
-$sql = "select * from servicos where id = :id or nome = :nome";
+$sql = "select * from servicos where id = :id";
 $cmd = $pdo->prepare($sql);
-$cmd->execute([":id"=>$id, ":nome"=>$nome]);
-$servicos = $cmd->fetchAll(PDO::FETCH_ASSOC);
-
-//$sql = "select * from servicos";
-//$cmd = $pdo->prepare($sql);
-//$cmd->execute();
-//$servicos = $cmd->fetchAll(PDO::FETCH_ASSOC);
-
-$sql = "select * from usuarios";
-$cmd = $pdo->prepare($sql);
-$cmd->execute();
-$usuarios = $cmd->fetchAll(PDO::FETCH_ASSOC);
-
+$cmd->execute(["id"=>$id]);
+$servicos = $cmd->fetchAll(PDO::FETCH_ASSOC); // Me retornará uma matriz associativa
+//var_dump($servicos); // Impressão na tela
+ 
 $sql = "select * from clientes";
 $cmd = $pdo->prepare($sql);
 $cmd->execute();
 $clientes = $cmd->fetchAll(PDO::FETCH_ASSOC);
+ 
+$sql = "select * from usuarios";
+$cmd = $pdo->prepare($sql);
+$cmd->execute();
+$usuarios = $cmd->fetchAll(PDO::FETCH_ASSOC);
+ 
+ 
+ 
+ 
 ?>
-
-
+ 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
+ 
 <head>
     <meta charset="UTF-8">
-    <title>Aula pdo <?php ?></title>
+    <title>Aula PDO PHP</title>
 </head>
+ 
 <body>
+ 
+    <form action="resform.php" method="post">
+        <input type="text" name="txtid" id="" >
+        <input type="password" name="txtsenha" id="" >
+        <button type="submit">Enviar</button>
+    </form>
+ 
     <h2>Lista de Serviços</h2>
-    <table border="1" cellpadding="5" >
-       <tr>
+ 
+    <table border=1 collpadding=10>
+        <tr>
             <th>ID</th>
             <th>Nome</th>
-            <th>Descrição</th>
+            <th>Descriçãp</th>
             <th>Preço</th>
             <th>Descontinuado</th>
-       </tr>
-       <?php foreach($servicos as $servico): ?>
-       
-        <tr>
-            <td><?php echo $servico['id']; ?></td>
-            <td><?php echo $servico['nome']; ?></td>
-            <td><?php echo $servico['descricao']; ?></td>
-            <td><?php echo $servico['preco']; ?></td>
-            <td><?php echo $servico['descontinuado']?"Sim":"Não"?></td>
         </tr>
-        <?php endforeach; ?>
+ 
+        <?php foreach ($servicos as $servico): ?>
+ 
+            <tr>
+                <td> <?= $servico['id']; ?> </td>
+                <td> <?= $servico['nome']; ?> </td>
+                <td> <?= $servico['descricao']; ?> </td>
+                <td> <?= $servico['preco']; ?> </td>
+                <td> <?= $servico['descontinuado'] ? "Sim" : "Não"    ?></td>
+ 
+            <?php endforeach; ?>
+            </tr>
     </table>
-    
-    <h3>Lista de usuarios</h3>
-    <table border="1" cellpadding="7">
+    <!-- ============================================================================================= -->
+ 
+    <!-- id, nome, email, senha, tipo, ativo, primeiro_login -->
+    <h2>Lista de Usuários</h2>
+ 
+    <table border=1 collpadding=10>
         <tr>
             <th>ID</th>
             <th>Nome</th>
@@ -60,43 +75,51 @@ $clientes = $cmd->fetchAll(PDO::FETCH_ASSOC);
             <th>Senha</th>
             <th>Tipo</th>
             <th>Ativo</th>
-            <th>Primeiro Login</th> 
+            <th>Primeiro Login</th>
         </tr>
-        <?php foreach($usuarios as $usuario): ?>
+ 
+        <?php foreach ($usuarios as $usuario): ?>
+ 
             <tr>
-            <td><?php echo $usuario['id']; ?></td>
-            <td><?php echo $usuario['nome']; ?></td>
-            <td><?php echo $usuario['email']; ?></td>
-            <td><?php echo $usuario['senha']; ?></td>
-            <td><?php echo $usuario['tipo']; ?></td>
-            <td><?php echo $usuario['ativo']; ?></td>
-            <td><?php echo $usuario['primeiro_login']; ?></td>
+                <td> <?= $usuario['id']; ?> </td>
+                <td> <?= $usuario['nome']; ?> </td>
+                <td> <?= $usuario['email']; ?> </td>
+                <td> <?= $usuario['senha']; ?> </td>
+                <td> <?= $usuario['tipo']? "Administrador":"Usuário"?> </td>
+                <td> <?= $usuario['ativo']? "Sim":"Não"?> </td>
+                <td> <?= $usuario['primeiro_login']; ?> </td>
+ 
+            <?php endforeach; ?>
             </tr>
-             <?php endforeach; ?>
     </table>
-
-    <h4>Lista de Clientes</h4>
-    <table border="1" cellpadding="7">
+ 
+<!-- ============================================================================================ -->
+ 
+        <h2>Lista de Clientes</h2>
+ 
+    <table border=1 collpadding=10>
         <tr>
             <th>ID</th>
-            <th>Id do user</th>
+            <th>Usuario_ID</th>
             <th>Telefone</th>
             <th>CPF</th>
         </tr>
-        <?php foreach($clientes as $cliente): ?>
+ 
+        <?php foreach ($clientes as $cliente): ?>
+ 
             <tr>
-            <td><?php echo $cliente['id']; ?></td>
-            <td><?php echo $cliente['usuario_id']; ?></td>
-            <td><?php echo $cliente['telefone']; ?></td>
-            <td><?php echo $cliente['cpf']; ?></td>
+                <td> <?= $clientes['id']; ?> </td>
+                <td> <?= $cliente['usuario_id']; ?> </td>
+                <td> <?= $cliente['telefone']; ?> </td>
+                <td> <?= $cliente['cpf']; ?> </td>
+ 
+            <?php endforeach; ?>
             </tr>
-             <?php endforeach; ?>
     </table>
-            <hr>
-    <form action="resform.php" method="post">
-        <input type="text" name="txtnome" id="">
-        <button type="submit">Enviar</button>
-    </form>
+ 
+<!-- ============================================================================================ -->
+ 
+ 
+ 
 </body>
-</html>
-
+ 
--- resform.php
+++ resform.php
@@ -1,4 +1,27 @@
 <?php
-$nome = $_POST['txtnome'];
-echo $nome;
+ 
+include "config/conexao.php";
+     if($_SERVER['REQUEST_METHOD']=="POST"){
+         echo "<h3>chamado pelo formulário (POST)</h3>";
+         $id = $_POST['txtid'];
+         $sql = "select id, nome from servicos where id = :id";
+         $cmd = $pdo->prepare($sql);
+         $cmd->execute(["id" => $id]);
+         $servicos = $cmd->fetchAll(PDO::FETCH_ASSOC);
+         var_dump($servicos);
+           
+    }
+if($_SERVER['REQUEST_METHOD']=="GET")
+    echo "<h3>chamado pela URL ou formulário method = 'GET'</h3>";
+    $idViaGet = $_GET['txtid'];
+    $sql = "select * from servicos where id = :id";
+    $cmd = $pdo->prepare($sql);
+    $cmd->execute([":id" => $idViaGet]);
+    $serviços = $cmd->fetch(PDO::FETCH_ASSOC);
+    var_dump($serviços);
 ?>
+ 
+ 
+<h2>Nome do Serviço: <?= $servico['nome'] ?></h2>
+ 
+ 
