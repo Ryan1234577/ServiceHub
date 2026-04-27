612e05a121b96d689909438bbf93242b78683fe9
Ryan Marcos da Silva Costa <ryanmarcosdasilvacosta@gmail.com>
2026-04-27 17:18:19 -0300

    Aula dia 27/04/2026, na aula de hoje, demos continuidade na atividade. Apenas adicionamos no arquivo ServicoSolicitacao os métodos necessários para a classe funcionar.

--- class/ServicoSolicitacao.php
+++ class/ServicoSolicitacao.php
@@ -13,6 +13,58 @@ include_once "config/conexao.php";
     {
     $this->pdo = obterPdo(); 
     }
- }
+
+     public function getServico_Id()
+        {
+        return $this->servico_id;
+         }
+  
+        public function setServico_Id(int $servico_id)
+         {
+        $this->servico_id = $servico_id;
+        }
+
+         public function getSolicitacao_Id()
+        {
+        return $this->solicitacao_id;
+         }
+  
+        public function setSolicitacao_Id(int $solicitacao_id)
+         {
+        $this->solicitacao_id = $solicitacao_id;
+        }
+
+         public function getData_Assoc()
+        {
+        return $this->data_assoc;
+         }
+  
+        public function setData_Assoc(int $data_assoc)
+         {
+        $this->data_assoc = $data_assoc;
+        }
+
+
+        //metodo listarServicosDaSolicitacao
+        public static function listarServicosDaSolicitacao(int $solicitacao_id): array{
+         $sql = "select se.*, ss.data_assoc from servico_solicitacao ss inner join servicos se on se.id = ss.servico_id where ss.solicitacao_id = :solicitacao_id";
+         $cmd = obterPdo()->prepare($sql);
+         $cmd->bindValue(":solicitacao_id", $solicitacao_id, PDO::PARAM_INT);
+         $cmd->execute();
+         return $cmd->fetchAll(PDO::FETCH_ASSOC);
+        }
+    
+
+         //metodo associar
+            public function associar(int $servico_id, int $solicitacao_id): bool
+    {
+        //$sql = "INSERT servico_solicitacao VALUES (1, 4, default)"
+        $sql = "INSERT servico_solicitacao VALUES (:servico_id, :solicitacao_id default)";
+        $cmd = obterPdo()->prepare($sql);
+        $cmd->bindValue(":servico_id",$servico_id ,":solicitacao_id", $solicitacao_id);
+        return $cmd->execute();
+    }
+    
+}
 
 ?>
